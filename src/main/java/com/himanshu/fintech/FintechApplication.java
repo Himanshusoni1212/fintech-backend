1badbb5e99da989062a6bd3d8d8162779a19024d
Himanshu soni <himanshusoni1921@gmail.com>
2026-03-08 17:20:09 +0530

    Add backend packages and HealthController

--- src/main/java/com/himanshu/fintech/FintechApplication.java
+++ src/main/java/com/himanshu/fintech/FintechApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FintechApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(FintechApplication.class, args);
 	}
 
